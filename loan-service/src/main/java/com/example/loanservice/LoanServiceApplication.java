26c493fbffea4c55351507e8aa742e5ff5dfc7a1
Muhammed Süha Demirel <suhademirel@Mac.home>
2025-05-16 00:46:29 +0300

    TPS-5: Start to develop service side

--- loan-service/src/main/java/com/example/loanservice/LoanServiceApplication.java
+++ loan-service/src/main/java/com/example/loanservice/LoanServiceApplication.java
@@ -1,4 +1,4 @@
-package com.personalProject.personal_project;
+package com.example.loanservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- loan-service/src/main/java/service/LoanService.java
+++ loan-service/src/main/java/service/LoanService.java
@@ -1,10 +1,16 @@
 package service;
 
+import controller.request.CreateLoanRequest;
+import entity.Customer;
 import entity.Loan;
+import entity.LoanInstallment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repository.CustomerRepository;
+import repository.LoanInstallmentRepository;
 import repository.LoanRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +19,26 @@ public class LoanService {
     @Autowired
     private LoanRepository loanRepository;
 
-    public Loan createLoan(Loan loan) {
-        return loanRepository.save(loan);
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private LoanInstallmentRepository loanInstallmentRepository;
+
+    public Loan createLoan(CreateLoanRequest request) {
+       Customer customer = customerRepository.findById(request.customerId()).orElseThrow();
+        if (request.amount() > customer.getCreditLimit()-customer.getUsedCreditLimit())
+            throw new RuntimeException();
+        Double installmentAmount = (request.amount()  * (1 + request.interestRate())) / request.installmentNumber();
+        Loan loan = loanRepository.save(loan)
+                List<LoanInstallment> installments = new ArrayList<>();
+                for(int i = 0; i < request.installmentNumber(); i++ ){
+                    LoanInstallment loanInstallment = new LoanInstallment(loan.getId(), installmentAmount);
+                    installments.add(loanInstallment);
+                }
+                loanInstallmentRepository.saveAll(installments);
+
+        return loanRepository.save(c);
     }
 
     public List<Loan> getLoansByCustomerId(Long customerId) {
@@ -23,5 +47,12 @@ public class LoanService {
 
     public List<Loan> getAllLoans() {
         return loanRepository.findAll();
+    }
+
+    public void payLoan(Long customerId, Long LoanId, Double paymentAmount) {
+        Loan loan = loanRepository.findById(LoanId).orElseThrow();
+        if (paymentAmount<loan.getAmount()/loan.getInstallmentNumber())
+            throw new RuntimeException();
+        List<LoanInstallment> installments = loanInstallmentRepository.findByLoanId(loan.getId())
     }
 }
